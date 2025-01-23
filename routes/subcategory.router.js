55f05512da3252a9312e03dc40f54cc092fdce71
NidhiGupta2003 <nidhijgupta27@gmail.com>
2025-01-23 23:04:50 +0530

    Delete routes directory

--- routes/subcategory.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const subcatRouter = express.Router();
-import * as subcatController from '../controller/subcategory.controller.js';
-subcatRouter.post("/save",subcatController.save);
-subcatRouter.get("/fetch",subcatController.fetch);
-subcatRouter.delete("/delete",subcatController.subcatDelete);
-subcatRouter.patch("/update",subcatController.subcatUpdate);
-export default subcatRouter;
