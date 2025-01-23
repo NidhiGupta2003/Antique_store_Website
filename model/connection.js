c9800af44501f9fbfad02f74d8e17f6ba71bcd96
NidhiGupta2003 <nidhijgupta27@gmail.com>
2025-01-23 23:04:36 +0530

    Delete model directory

--- model/connection.js
+++
@@ -1,4 +0,0 @@
-import mongoose from "mongoose";
-const url = "mongodb://localhost:27017/myantiqueshop";//database Name
-mongoose.connect(url);//connecting of database
-console.log("Successfully connected to mongod database");
